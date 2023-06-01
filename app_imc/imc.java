393af0751e8e275a72826bed7da4539618d69d86
Gabriel Costa de Moraes <121687389+GabrielCM16@users.noreply.github.com>
2023-06-01 09:06:39 -0300

    Update imc.java

--- app_imc/imc.java
+++ app_imc/imc.java
@@ -1,4 +1,4 @@
-package com.example.topicoshelo;
+package com.example.forca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,14 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Random;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView Resultado;
-    private EditText editPeso;
-    private EditText editAltura;
-    private EditText editIdade;
-
+    public EditText entradaTexto;
+    public TextView SaidaVida;
+    public TextView SaidaPalavras;
 
 
     @Override
@@ -21,82 +26,157 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Resultado = findViewById(R.id.Resultado);
+        entradaTexto = findViewById(R.id.entrada);
 
-        editPeso = findViewById(R.id.editPeso);
-        editAltura = findViewById(R.id.editAltura);
-        editIdade = findViewById(R.id.editIdade);
+        SaidaPalavras = findViewById(R.id.SaidaPalavra);
 
+        SaidaVida = findViewById(R.id.SaidaVidas);
+    }
+
+    final static public String palavras[] = {"RATO", "SAPO", "MARRECO", "PANELA", "CAVALO", "MACHADO", "CEREJA", "LIMAO", "LARANJA", "LELIS"};
+
+    static String palavraSoteada = palavras[sortea()];
+
+    static int im = palavraSoteada.length();
+
+    final static String letrasUsadas[] = new String[im];
+
+    public static int vidas = 0;
+
+    static int dificuldade = 10;
+
+    static int aux = 0;
+
+    public static String palavra_incompleta = "";
+
+    public static String palavra_aux = "";
+
+
+
+    public void reiniciarJogo(View view) {
+
+        SaidaVida.setText(vidas);
+
+        palavraSoteada = palavras[sortea()];
+
+        vidas = 0;
+
+        aux = 0;
+
+        dificuldade = 10;
+
+        palavra_incompleta = "";
+
+        palavra_aux = "";
+
+    }
+
+    public void Jogar(View view){
+
+
+        String n_vida = Integer.toString(10 - vidas);
+        SaidaVida.setText("Chances restantes = "+ (n_vida));
+
+        SaidaPalavras.setText(palavraSoteada);
+
+        palavra_aux = "";
+
+        StringBuilder sb = new StringBuilder(palavra_incompleta);
+
+
+        String letra = entradaTexto.getText().toString();
+
+        boolean repete = false;
+
+        if (vidas >= dificuldade){
+            String textoo = "Voce perdeu o jogo!";
+            abrirToast(textoo);
+        } else {
+            if (letra.length() == 1){
+                //é uma letra adiciona no vetor somente se nao estiver
+
+                //verificando se a letra inserida ja foi adicionada
+//                for(int i = 0; i <= letrasUsadas.length; i++){
+//
+//                    String letraa = letrasUsadas[i];
+//
+//                    if (letraa.equals(letra.toUpperCase())){
+//                        //letra ja foi usada
+//                        //precisa dar erro
+//                        String textoo = "Letra ja inserida!";
+//                        abrirToast(textoo);
+//
+//                        vidas += 1;
+//
+//                        repete = true;
+//                        i = letrasUsadas.length;
+//                    } else{
+//                        continue;
+//                    }
+//                }
+                if (!repete){ //se a letra nao repete
+
+
+
+                    for(int i = 0; i < palavraSoteada.length(); i++){
+
+                        char letraDaStr = palavraSoteada.charAt(i);
+
+                        if(letra.equals(letraDaStr)){
+                            sb.setCharAt(i, letraDaStr);
+                        } else {
+                            palavra_aux += '_';
+                        }
+
+                    }
+
+
+                    //adicionar a letra no vetor de letras usadas
+                    letrasUsadas[aux] = letra.toUpperCase();
+                    aux += 1;
+                }
+
+            } else {
+                //não é uma letra precisa dar erro
+                String textoo = "Insira uma letra valida";
+                abrirToast(textoo);
+            }
+
+
+
+        }
+
+
+        //Verificando se ja ganhou
+
+        if(palavraSoteada.equals(palavra_incompleta)){
+            String textoo = "Voce Ganhouuu";
+            abrirToast(textoo);
+        }
+
+        n_vida = Integer.toString(10 - vidas);
+        SaidaVida.setText("Chances restantes = "+ (n_vida));
+        SaidaPalavras.setText(palavra_incompleta);
 
 
     }
 
-    public void processar (View view){
 
 
-        String Peso = editPeso.getText().toString();
-        String Altura = editAltura.getText().toString();
-        String Idade = editIdade.getText().toString();
+    public static int sortea(){
+        Random gerador = new Random();
 
-        int peso = Integer.parseInt(Peso);
-        int altura = Integer.parseInt(Altura);
-        int idade = Integer.parseInt(Idade);
+        int num1 = gerador.nextInt(palavras.length);
+        return num1;
 
-        float aaltura = (float) altura/100;
+    }
 
-        double IMC = peso / Math.pow(aaltura, 2);
-
-        String faixaEtaria = "";
-        String classificacao = "";
-
-        if (idade > 65){
-            faixaEtaria = "Idoso";
-        } else {
-            faixaEtaria = "Adulto";
-        }
-
-        if (faixaEtaria == "Adulto"){
-
-            if (IMC < 18.5){
-                classificacao = "Baixo peso";
-            } else if(IMC > 18.5 && IMC <= 24.9){
-                classificacao = "Peso normal";
-            } else if (IMC >= 25 && IMC <= 29.9){
-                classificacao = "Excesso de peso";
-            } else if(IMC >= 30 && IMC <= 34.9){
-                classificacao = "Obesidade de Classe 1";
-            } else if(IMC >= 35 && IMC <= 39.9){
-                classificacao = "Obesidade de Classe 2";
-            } else if (IMC >= 40){
-                classificacao = "Obesidade de Classe 3";
-            }
-
-
-        } else {
-
-
-
-
-            if (IMC <= 22) {
-                classificacao = "Baixo peso";
-            }
-
-            if (IMC > 22 && IMC < 27) {
-                classificacao = "Adequado ou eutrófico";
-            }
-
-            if (IMC >= 27) {
-                classificacao = "Sobrepeso";
-            }
-
-
-
-
-        }
-
-        Resultado.setText("Resultado: " + faixaEtaria + " " + aaltura +  "m " + peso + "Kg \n" + "IMC: " + IMC  + "\n" + classificacao + "\n" );
-
-
+    public void abrirToast(String texto){
+        Toast.makeText(
+                getApplicationContext(),
+                texto,
+                Toast.LENGTH_LONG
+        ).show();
     }
 
 }
