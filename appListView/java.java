b52bc8f89c57add95f3b2d2b1a9edac46021e796
Gabriel Costa de Moraes <121687389+GabrielCM16@users.noreply.github.com>
2023-07-27 11:11:30 -0300

    Update java.java

--- appListView/java.java
+++ appListView/java.java
@@ -3,9 +3,12 @@ package com.example.appaula;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,5 +47,14 @@ public class MainActivity extends AppCompatActivity {
                 itens // lista a ser passada
         );
         listLocais.setAdapter(adapter);
+
+        //adiciona o click na lista
+        listLocais.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String valorSelecionado = listLocais.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(), valorSelecionado, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
