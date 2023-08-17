22b8357214dfb4a3a0bd55e4b914a3585797cb43
Gabriel Costa de Moraes <121687389+GabrielCM16@users.noreply.github.com>
2023-08-17 08:02:19 -0300

    Create MainActivity.java

---
+++ RecyclerView/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.recyclerview.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.example.recyclerview.R;
+import com.example.recyclerview.adapter.Adapter;
+import com.example.recyclerview.model.Filme;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<Filme> listaFilmes = new ArrayList<>();
+
+
+    public void criarFilmes(){
+        Filme filme = new Filme("Homem Aranha", "aventura", "2018");
+        this.listaFilmes.add(filme);
+        filme = new Filme("Mulher Maravilha", "aventura", "2019");
+        this.listaFilmes.add(filme);
+        filme = new Filme("O Senhor dos Aneis", "ficção", "2001");
+        this.listaFilmes.add(filme);
+        filme = new Filme("Matrix", "ficção", "2002");
+        this.listaFilmes.add(filme);
+        filme = new Filme("Poderoso Chefão", "drama", "1972");
+        this.listaFilmes.add(filme);
+        filme = new Filme("Doze Homens e uma Sentença", "drama", "1957");
+        this.listaFilmes.add(filme);
+        filme = new Filme("Forrest Gump", "drama", "1994");
+        this.listaFilmes.add(filme);
+        filme = new Filme("Dois Papas", "drama", "2019");
+        this.listaFilmes.add(filme);
+        filme = new Filme("O Regresso", "drama", "2015");
+        this.listaFilmes.add(filme);
+        filme = new Filme("A Teoria de Tudo", "drama", "2014");
+        this.listaFilmes.add(filme);
+        filme = new Filme("Django Livre", "ação", "2012");
+        this.listaFilmes.add(filme);
+        filme = new Filme("Era uma vez em Hollywood", "comédia", "2019");
+        this.listaFilmes.add(filme);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        //listagem de filmes
+        this.criarFilmes();
+
+        // Configurar adapter;
+        Adapter adapter = new Adapter(listaFilmes);
+
+
+        //Configurar RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
+        recyclerView.setAdapter(adapter); //criar adapter
+
+
+    }
+}
